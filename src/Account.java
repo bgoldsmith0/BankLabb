e3c54090420d33c6f38a30e3c4df3a4acdd7c687
Benny G <bgoldsmith0@gmail.com>
2015-09-09 11:50:09 -0700

    ayy

--- src/Account.java
+++ src/Account.java
@@ -2,4 +2,21 @@
  * Created by movec_000 on 2015-09-09.
  */
 public class Account {
+
+    public int iD;
+    public int transactions = 0;
+    public double balance = 0;
+
+    public Account(int iD, double balance) {
+        this.iD = iD;
+        this.balance = balance;
+    }
+
+    public void deposit(double in) {
+        balance = balance + in;
+    }
+
+    public void withdraw(double out) {
+        balance = balance - out;
+    }
 }
