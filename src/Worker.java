c6bd691e40dcadf213b17bc01688c12f67ff8504
William Ruppenthal <williamruppenthal42@gmail.com>
2015-09-10 12:09:50 -0700

    It's done except for the nullptr exception round 2

--- src/Worker.java
+++ src/Worker.java
@@ -4,40 +4,38 @@ import java.util.ArrayList;
  * Created by movec_000 on 2015-09-09.
  */
 public class Worker implements Runnable {
-    public static ArrayList<Account> acts;
-    public Transaction toDeal;
-    public ArrayList<Account> accounts;
+    public Transaction t;
+    public Bank b;
     public int workerID;
 
-    public Worker(int is, ArrayList<Account> f) {
+    public Worker(int is, Bank ba) {
         workerID = is;
-        accounts = f;
+        b=ba;
 
     }
 
     public void run() {
 
-        while (accounts.get(t.getFrom()).isInUse()) {
+        while (b.accounts.get(t.getFrom()).isInUse()) {
 
         }
-        accounts.get(t.getFrom()).withdraw(t.getAmount());
-        while (accounts.get(t.getTo()).isInUse()) {
+        b.accounts.get(t.getFrom()).withdraw(t.getAmount());
+        while (b.accounts.get(t.getTo()).isInUse()) {
 
         }
-        accounts.get(t.getTo()).deposit(t.getAmount());
+        b.accounts.get(t.getTo()).deposit(t.getAmount());
 
     }
-    }
 
-    public void doT(Transaction t, ArrayList<Account> accounts) {
-        while (accounts.get(t.getFrom()).isInUse()) {
-
-        }
-        accounts.get(t.getFrom()).withdraw(t.getAmount());
-        while (accounts.get(t.getTo()).isInUse()) {
-
-        }
-        accounts.get(t.getTo()).deposit(t.getAmount());
-
-    }
+//    public void doT(Transaction t, ArrayList<Account> accounts) {
+//        while (accounts.get(t.getFrom()).isInUse()) {
+//
+//        }
+//        accounts.get(t.getFrom()).withdraw(t.getAmount());
+//        while (accounts.get(t.getTo()).isInUse()) {
+//
+//        }
+//        accounts.get(t.getTo()).deposit(t.getAmount());
+//
+//    }
 }
