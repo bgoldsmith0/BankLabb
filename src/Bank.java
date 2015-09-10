849790e027049519f8301939939eeac7fc11a58a
William Ruppenthal <williamruppenthal42@gmail.com>
2015-09-10 12:07:53 -0700

    It's done except for the nullptr exception.

--- src/Bank.java
+++ src/Bank.java
@@ -3,23 +3,40 @@ import java.util.*;
 import java.io.File;
 
 public class Bank{
-    public static ArrayBlockingQueue<Transaction> list;
-    public static ArrayList<Account> accounts;
+    public ArrayBlockingQueue<Transaction> list;
+    public ArrayList<Account> accounts;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(new File("small.txt"));
-        int og,ng,amount;
-        for(int c=0;c<20;c++)
-            accounts.add(new Account(c));
+        Bank bank = new Bank();
 
-        for(int c=0;c<8;c++)
-            new Thread(new Worker(c)).start();
+        try{
+            Scanner scanner = new Scanner(new File("small.txt"));
 
-        while(scanner.hasNextInt()){
-            og = scanner.nextInt();
-            ng = scanner.nextInt();
-            amount = scanner.nextInt();
-            list.add(new Transaction(og,ng,amount));
-        }
+            int og,ng,amount;
+            for(int c=0;c<20;c++)
+                bank.accounts.add(new Account(c));
+
+            for(int c=0;c<8;c++)
+                new Thread(new Worker(c,bank)).start();
+
+            while(scanner.hasNextInt()){
+                og = scanner.nextInt();
+                ng = scanner.nextInt();
+                amount = scanner.nextInt();
+                bank.list.add(new Transaction(og,ng,amount));
+            }
+        }catch(java.io.FileNotFoundException e){}
+
+        System.out.print(bank.toString());
+    }
+
+    public String toString(){
+        String s="";
+
+        for(Account a:this.accounts)
+            s+="acct:"+a.iD+" bal:"+a.balance+" trans:"+a.transactions+"" +
+                    "\n";
+
+        return s;
     }
 }
