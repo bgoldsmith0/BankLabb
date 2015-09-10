93b03ab4ffcb145c356f035161445e78a818c431
Benny G <bgoldsmith0@gmail.com>
2015-09-10 11:45:05 -0700

    f

--- src/Worker.java
+++ src/Worker.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 /**
  * Created by movec_000 on 2015-09-09.
  */
-public class Worker {
+public class Worker implements Runnable {
     public Transaction toDeal;
     public ArrayList<Account> currentAccounts;
     public int workerID;
@@ -13,7 +13,19 @@ public class Worker {
 
     }
 
-    public void doT(Transaction t, ArrayList<Account>) {
+    public void run() {
+
+    }
+
+    public void doT(Transaction t, ArrayList<Account> accounts) {
+        while (accounts.get(t.getFrom()).isInUse()) {
+
+        }
+        accounts.get(t.getFrom()).withdraw(t.getAmount());
+        while (accounts.get(t.getTo()).isInUse()) {
+
+        }
+        accounts.get(t.getTo()).deposit(t.getAmount());
 
     }
 }
