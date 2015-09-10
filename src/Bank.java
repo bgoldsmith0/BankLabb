b49b4aa3d57f486c3b2808efd0b9575785a1933d
wruppenthal <williamruppenthal42@gmail.com>
2015-09-10 11:40:57 -0700

    Update Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,21 @@
-/**
- * Created by movec_000 on 2015-09-09.
- */
-public class Bank {
+package com.company;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.*;
+import java.io.File;
+
+public class Bank{
+    public static ArrayBlockingQueue<Transaction> list;
+    public static
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new File("tall.txt"));
+        int og,ng,amount;
+        
+        while(scanner.hasNextInt()){
+            og = scanner.nextInt();
+            ng = scanner.nextInt();
+            amount = scanner.nextInt();
+            list.add(new Transaction(og,ng,amount));
+        }
+    }
 }
